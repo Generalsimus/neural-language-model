ef02032c322c2814e32e82b5e47c74124a4269dc
Generalsimus <totorotomoney@gmail.com>
2023-06-21 15:44:37 +0400

    update

--- main.go
+++ main.go
@@ -4,40 +4,65 @@ import (
 	"fmt"
 	"model/model"
 	"model/utils"
+	// "model/utils"
 )
 
 func main() {
-	fmt.Println("SS")
-	// input := [][]float64{
-	// 	[]float64{2, 5},
-	// }
+	// input := 0.85
+
+	// desiredOutput := 0.7
+
+	// error := desiredOutput - input
+	// delta := error * utils.Derivative(input)
+
+	// learnRate := 0.01
+	// fmt.Println("error: ", error)
+	// fmt.Println("Derivative: ", utils.Derivative(input))
+	// fmt.Println("learnRate: ", learnRate)
+	// fmt.Println("delta: ", delta)
+	// fmt.Println("n2: ", learnRate*delta)
+	input := 1.5
+
+	width := 0.8
+
+	output := utils.Sigmoid(input * width)
+	outputDer := utils.Derivative(output)
+
+	fmt.Println(input, width, output, outputDer, output-outputDer)
+	// return
 	net := model.CreateModel([]int{
 		3,
-		// 2,
+		16,
 		2,
 	}, 0.01)
+	input1 := []float64{0.1, 0.2, 0.3}
+	output1 := []float64{0.1, 0.0}
 
-	for i := 0; i < 5; i++ {
+	input2 := []float64{0.1, 0.5, 0.3}
+	output2 := []float64{0, 0.1}
 
-		net.Train([]float64{1, 2, 3}, []float64{0, 1})
-		net.Train([]float64{1, 5, 3}, []float64{1, 0})
-	}
-	fmt.Println(utils.Sigmoid(0.4978321736604644*1 + 0.4956831970068629*2 + 0.4934965209813932*3))
+	// for i := 0; i < 400000; i++ {
+	// 	// fmt.Println("LEARN1")
+	// net.Train(input1, output1)
+
+	// // 	// fmt.Println("LEARN2")
+	net.Train(input2, output2)
+	// }
 
 	fmt.Println("SS2: ", net)
-	fmt.Println("EEEE: ", utils.Sigmoid(10)*(1-utils.Sigmoid(10)), 0.5*10)
 
-	forward1 := net.Forward([]float64{1, 2, 3})
+	forward1 := net.Forward(input1)
 	m1 := FindMaxIndex(forward1)
 
-	fmt.Println("Forward1: ", forward1, m1, forward1[1] > forward1[0])
+	fmt.Println("Forward1: ", forward1, m1, FindMaxIndex(forward1) == FindMaxIndex(output1), output1)
 
-	forward2 := net.Forward([]float64{1, 5, 3})
+	forward2 := net.Forward(input2)
 	m2 := FindMaxIndex(forward2)
 
-	fmt.Println("Forward2: ", forward2, m2, forward2[1] > forward2[0])
+	fmt.Println("Forward2: ", forward2, m2, FindMaxIndex(forward2) == FindMaxIndex(output2), output2)
 
 }
+
 func FindMaxIndex[T int | uint | float64 | int64 | float32](a []T) int {
 	maxIndex := 0
 	max := a[maxIndex]
--- model/layer.go
+++ model/layer.go
@@ -25,7 +25,8 @@ func (l *Layer) Fill(inputSize int, outPutSize int) {
 	for i := 0; i < outPutSize; i++ {
 		inputWidths := []float64{}
 		for i := 0; i < inputSize; i++ {
-			inputWidths = append(inputWidths, 0.5)
+			inputWidths = append(inputWidths, 1)
+			// inputWidths = append(inputWidths, rand.Float64())
 		}
 
 		layerWidths = append(layerWidths, inputWidths)
@@ -52,8 +53,8 @@ func (l *Layer) Forward(inputs []float64) []float64 {
 		for index, inputWidth := range inputWidths {
 			newInput += inputWidth * inputs[index]
 		}
-		nextLayerInputs[layerWidthsIndex] = utils.Sigmoid(newInput)
-		// nextLayerInputs[layerWidthsIndex] = utils.Sigmoid(newInput + l.Biases[layerWidthsIndex])
+		// nextLayerInputs[layerWidthsIndex] = utils.Sigmoid(newInput)
+		nextLayerInputs[layerWidthsIndex] = utils.Sigmoid(newInput + l.Biases[layerWidthsIndex])
 	}
 	// fmt.Println("FORWARD: ", nextLayerInputs)
 	if l.NextLayer == nil {
@@ -64,6 +65,7 @@ func (l *Layer) Forward(inputs []float64) []float64 {
 }
 
 func (l *Layer) Train(inputs []float64, desiredOutputs []float64, learnRate float64) []float64 {
+
 	// FORWARD ////////////////////////////////////////////
 	layerWidths := l.LayerWidths
 
@@ -73,8 +75,8 @@ func (l *Layer) Train(inputs []float64, desiredOutputs []float64, learnRate floa
 		for index, inputWidth := range inputWidths {
 			newInput += inputWidth * inputs[index]
 		}
-		nextLayerInputs[layerWidthsIndex] = utils.Sigmoid(newInput)
-		// nextLayerInputs[layerWidthsIndex] = utils.Sigmoid(newInput + l.Biases[layerWidthsIndex])
+		// nextLayerInputs[layerWidthsIndex] = utils.Sigmoid(newInput)
+		nextLayerInputs[layerWidthsIndex] = utils.Sigmoid(newInput + l.Biases[layerWidthsIndex])
 	}
 	// fmt.Println("FORWARD: ", nextLayerInputs)
 
@@ -82,31 +84,43 @@ func (l *Layer) Train(inputs []float64, desiredOutputs []float64, learnRate floa
 	var nextLayerDeltaOutput []float64
 	if l.NextLayer == nil {
 		nextLayerDeltaOutput = make([]float64, len(nextLayerInputs))
+		// errorCost := 0.00
 		for index, desiredOutput := range desiredOutputs {
-			inputValue := nextLayerInputs[index]
-			nextLayerDeltaOutput[index] = inputValue - desiredOutput
-			// nextLayerDeltaOutput[index] = utils.Derivative(inputValue) * (desiredOutput - inputValue)
+			nextInput := nextLayerInputs[index]
+			// errorCost += (desiredOutput - nextInput)
+
+			nextLayerDeltaOutput[index] = (desiredOutput - nextInput) * utils.Derivative(nextInput)
 		}
+		// fmt.Println("errorCost: ", errorCost)
 	} else {
 		nextLayerDeltaOutput = l.NextLayer.Train(nextLayerInputs, desiredOutputs, learnRate)
 	}
 
 	///////////////////////////////////////////////////////
 	deltaOutput := make([]float64, len(inputs))
+	// momentum := 0.00
 
 	for inputIndex, input := range inputs {
-		var deltaInput float64 = 0
-		derivativeInput := utils.Derivative(input)
+		delta := 0.00
+		for widthsIndex, widths := range l.LayerWidths {
+			nextLayerDelta := nextLayerDeltaOutput[widthsIndex]
+			width := widths[inputIndex]
 
-		for nextLayerDeltaIndex, nextLayerDelta := range nextLayerDeltaOutput {
-			width := l.LayerWidths[nextLayerDeltaIndex][inputIndex]
+			// error += outputNode.Error * outputNode.Input[node].Weight * (node.Output * (1.0 - node.Output))
+			delta += (nextLayerDelta * width)
 
-			deltaInput += derivativeInput * (width * nextLayerDelta)
+			// outputNode.Input[node].Weight += m_learningRate * outputNode.Error * node.Output;
+			widths[inputIndex] += learnRate * nextLayerDelta * input
+			// nextLayerInputs[widthsIndex]
 
-			// fmt.Println("widthsIndex: ", widthsIndex, " inputIndex: ", inputIndex)
-			l.LayerWidths[nextLayerDeltaIndex][inputIndex] = (learnRate * nextLayerDelta * input) + width
+			// neuron.bias.weight += neuron.bias.delta * learningRate;
+			// m_learningRate * outputNode.Error * outputNode.Bias.Weight
+			// l.Biases[widthsIndex] += nextLayerDelta * learnRate
+			l.Biases[widthsIndex] += nextLayerDelta * learnRate
+			// l.Biases[widthsIndex] += (nextLayerDelta * l.Biases[widthsIndex]) * learnRate
 		}
-		deltaOutput[inputIndex] = deltaInput
+
+		deltaOutput[inputIndex] = delta * utils.Derivative(input)
 	}
 
 	return deltaOutput
